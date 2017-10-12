88e336e6c14c1337b8e6414a4980e8d309e7369b
souija <3349375698qq.com>
2017-10-12 22:18:26 +0800

    创建控制台应用程序，方便后面的代码练习。

---
+++ CodeTest/CodeTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "我现在要提交代码到我的github版本库中。";
+        }
+    }
+}
