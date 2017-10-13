c0a3b6483cca43c2392e78a1bc1979689e9c5514
Souija <1729137243@qq.com>
2017-10-13 14:58:01 +0800

    准备模板

--- CodeTest/CodeTest/Program.cs
+++ CodeTest/CodeTest/Program.cs
@@ -10,9 +10,11 @@ namespace CodeTest
     {
         static void Main(string[] args)
         {
-            string s = "我现在要提交代码到我的github版本库中。";
-            string s1 = "我又来提交测试了。aaaaaa";
-            string s2 = "不在宿舍里做的修改。sss ";
+            //string s = "我现在要提交代码到我的github版本库中。";
+            //string s1 = "我又来提交测试了。aaaaaa";
+            //string s2 = "不在宿舍里做的修改。sss ";
+            Console.WriteLine("hello world.");
+            Console.ReadKey();
             
         }
     }
--- CodeTest/Common/Helper/TaskHelper.cs
+++ CodeTest/Common/Helper/TaskHelper.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+* APM  异步编程模型，Asynchronous Programming Model
+* EAP  基于事件的异步编程模式，Event-based Asynchronous Pattern
+* TAP  基于任务的异步编程模式，Task-based Asynchronous Pattern
+* TPL  任务并行库，Task Parallel Library
+* 
+ */
 namespace Common.Helper
 {
     public class TaskHelper
