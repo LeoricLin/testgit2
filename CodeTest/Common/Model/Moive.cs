e5b6bb69b22c161d025bd5699afa09e2f90838ba
Souija <1729137243@qq.com>
2017-10-13 17:25:55 +0800

    1

---
+++ CodeTest/Common/Model/Moive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Model
+{
+    public class Moive:IComparable
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            //compareto方法是比较两个操作时的大小， 当前面一个操作数和第二个操作一样大时，返回0，如果第一个比第二个小返回-1，如果第一个比第二个大返回1
+            return this.Price.CompareTo(((Moive)obj).Price);
+        }
+        public Moive(string name,decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+    }
+}
